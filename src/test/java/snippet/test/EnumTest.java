2b9b839a442112367658e7488ab46317c08c9761
Gerrit Schrader <gerrit.schrader@gmail.com>
2018-02-06 08:21:32 +0100

    Junit5 added to launch config

--- src/test/java/snippet/test/EnumTest.java
+++ src/test/java/snippet/test/EnumTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import snippet.Enum;
 
-class EnumsTest {
+class EnumTest {
 	@Test
 	void test_1() {
 		Enum media = Enum.ENERGY;
@@ -20,8 +20,8 @@ class EnumsTest {
 	@Test
 	void test_2() {
 		Enum media = Enum.GAS;
-		assertEquals(0, media.ordinal());
-		assertEquals("ENERGY", media.name());
-		assertTrue(Enum.isValid(0));
+		assertEquals(1, media.ordinal());
+		assertEquals("GAS", media.name());
+		assertTrue(Enum.isValid(1));
 	}
 }
