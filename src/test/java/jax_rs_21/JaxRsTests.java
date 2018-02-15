94090c1d58709f5a387cb23c98262a99c1680250
Gerrit Schrader <gerrit.schrader@infinit-services.de>
2018-02-15 17:57:02 +0100

    vertx-junit5

--- src/test/java/jax_rs_21/JaxRsTests.java
+++ src/test/java/jax_rs_21/JaxRsTests.java
@@ -10,20 +10,20 @@ import javax.ws.rs.client.InvocationCallback;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.junit.runner.RunWith;
 
 import io.vertx.core.Vertx;
-import io.vertx.ext.unit.Async;
-import io.vertx.ext.unit.TestContext;
-import io.vertx.ext.unit.junit.VertxUnitRunner;
+import io.vertx.core.VertxOptions;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
 
 public class JaxRsTests {
 
@@ -49,47 +49,46 @@ public class JaxRsTests {
 
   @Nested
   @TestInstance(Lifecycle.PER_CLASS)
-  @RunWith(VertxUnitRunner.class)
+  @ExtendWith(VertxExtension.class)
   static class AsyncTests {
+
     private final WebTarget target = ClientBuilder.newClient().target("http://jsonplaceholder.typicode.com");
+
     private Vertx vertx;
 
-    @BeforeAll
-    void setUp() {
-      vertx = Vertx.vertx();
+    @BeforeEach
+    void prepare() {
+      vertx = Vertx.vertx(new VertxOptions().setMaxEventLoopExecuteTime(1000).setPreferNativeTransport(true)
+          .setFileResolverCachingEnabled(true));
     }
 
-    @AfterAll
-    void tesDown(TestContext context) {
-      vertx.close(context.asyncAssertSuccess());
+    @AfterEach
+    void cleanup() {
+      vertx.close();
     }
 
     @Test
-    void testAsync(TestContext context) throws InterruptedException, ExecutionException {
-      final Async async = context.async();
+    void testAsync(Vertx vertx, VertxTestContext testContext) throws InterruptedException, ExecutionException {
       Future<Response> future = target.path("/posts/1").request().async().get();
       Response resp = future.get();
-      context.assertEquals(Response.Status.OK.getStatusCode(), resp.getStatus());
-      context.assertEquals(Response.Status.OK.getReasonPhrase(), resp.getStatusInfo().getReasonPhrase());
-      async.complete();
-
+      assertEquals(Response.Status.OK.getStatusCode(), resp.getStatus());
+      assertEquals(Response.Status.OK.getReasonPhrase(), resp.getStatusInfo().getReasonPhrase());
+      testContext.completeNow();
     }
 
     @Test
-    void testAsyn2(TestContext context) throws InterruptedException, ExecutionException {
-      final Async async = context.async();
+    void testAsyn2(Vertx vertx, VertxTestContext testContext) throws InterruptedException, ExecutionException {
       InvocationCallback<Post> callback = new InvocationCallback<Post>() {
         @Override
         public void completed(Post post) {
-          context.assertEquals(1, post.userId);
-          context.assertEquals(1, post.id);
-          async.complete();
+          assertEquals(1, post.userId);
+          assertEquals(1, post.id);
+          testContext.completeNow();
         }
 
         @Override
         public void failed(Throwable throwable) {
-          context.asyncAssertFailure();
-          async.complete();
+          testContext.completeNow();
         }
       };
       target.path("/posts/1").request().async().get(callback);
