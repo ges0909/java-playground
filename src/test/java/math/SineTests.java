269d1bbff335ad32113bfc33f8196fec10b6c9ad
Gerrit Schrader <gerrit.schrader@gmail.com>
2018-08-18 12:29:57 +0200

    write generated data to file

--- src/test/java/math/SineTests.java
+++ src/test/java/math/SineTests.java
@@ -1,10 +1,15 @@
 package math;
 
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.text.DecimalFormat;
-import java.time.Instant;
 import java.time.LocalDate;
-import java.time.ZoneId;
+import java.time.LocalTime;
+import java.time.ZoneOffset;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -12,13 +17,15 @@ import java.util.Map;
 import java.util.Random;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 
 class SineTests {
+
+  private static final String home = System.getProperty("user.home");
+  private static final Path outputDir = Paths.get(home + "/Desktop/testOutput");
 
   private void trace(Double[] values) {
     final DecimalFormat df = new DecimalFormat("###"/* "###.00" */);
@@ -31,8 +38,36 @@ class SineTests {
     System.out.println();
   }
 
+  private void scilab(Map<Long, Double> samples) throws IOException {
+    Path path = Paths.get(home + "/Desktop/sine.sci");
+
+    final StringBuffer xbuf = new StringBuffer();
+    LongStream.range(0, samples.size()).forEach(l -> xbuf.append(l).append(","));
+    xbuf.setLength(xbuf.length() - 1);
+    String xvar = "x=[" + xbuf.toString() + "];" + "\n";
+    Files.write(path, xvar.getBytes());
+
+    final StringBuffer ybuf = new StringBuffer();
+    samples.values().forEach(v -> ybuf.append(v).append(","));
+    ybuf.setLength(ybuf.length() - 1);
+    String yvar = "y=[" + ybuf.toString() + "];" + "\n";
+    Files.write(path, yvar.getBytes(), StandardOpenOption.APPEND);
+
+    String plot = "plot(x,y);" + "\n";
+    Files.write(path, plot.getBytes(), StandardOpenOption.APPEND);
+  }
+
+  private void writeToFile(Path outputFile, Map<Long, Double> samples) throws IOException {
+    StringBuffer sb = new StringBuffer();
+    for (Map.Entry<Long, Double> sample : samples.entrySet()) {
+      String line = sample.getKey() + " " + sample.getValue() + "\n";
+      sb.append(line);
+    }
+    Files.write(outputFile, sb.toString().getBytes());
+  }
+
   @Test
-  void generateConsolidatedData() {
+  void testGenerateConsolidatedData() {
     final int secondsPerMonth = 64; // 2_592_000; // 30 * 24 * 3600;
     final int standardDeviation = 3; // 68%: 3, 95%: 6, >100%: ~9 => use 10 to avoid negative values
     final Function<Long, Double> sine1 = (t) -> 5 * Math.sin(t) + 5 + 10;
@@ -67,7 +102,7 @@ class SineTests {
   }
 
   @Test
-  void generateConsolidatedData2() {
+  void testGenerateConsolidatedData2() {
     final Random random = new Random();
     final int secondsPerMonth = 64; // 2_592_000; // 30 * 24 * 3600;
     final int standardDeviation = 3; // 68%: 3, 95%: 6, >100%: ~9 => use 10 to avoid negative values
@@ -89,37 +124,42 @@ class SineTests {
   }
 
   @Test
-  void generateConsolidatedData3() {
+  public void testGenerator() throws IOException {
+
     final Random random = new Random();
     final int standardDeviation = 3; // 68%: 3, 95%: 6, >100%: ~9 => use 10 to avoid negative values
     final Function<Long, Double> sine1 = (t) -> 5 * Math.sin(t) + 5 + (3 * standardDeviation);
-    final Function<Long, Double> sine2 = (t) -> 300 * Math.sin(t / 8 /*3600*/) + 300 + (3 * standardDeviation);
+    final Function<Long, Double> sine2 = (t) -> 300 * Math.sin(t / 8 /* 3600 */) + 300 + (3 * standardDeviation);
 
-    final Instant beginInstant =  Instant.parse("2018-08-01T00:00:00Z");
-    final long begin = beginInstant.getEpochSecond();
-    final long end = begin + 1024; // Instant.parse("2018-08-31T23:59:59Z").getEpochSecond();
+    LocalDate startDate = LocalDate.parse("2018-08-01");
+    LocalDate endDate = LocalDate.parse("2018-08-31");
 
-    LocalDate.ofInstant(beginInstant, ZoneId.systemDefault());
+    LocalDate curDate = startDate;
+    while (curDate.isBefore(endDate) || curDate.isEqual(endDate)) {
 
-    long size = (end - begin) / 100; // 1% anomalies
-    List<Long> anomaliesAt = random.longs(size, begin, end).boxed().collect(Collectors.toList());
+      long atBeginOfDay = curDate.atTime(LocalTime.MIN).toEpochSecond(ZoneOffset.of("+2"));
+      long atEndOfDay = curDate.atTime(LocalTime.MAX).toEpochSecond(ZoneOffset.of("+2"));
 
-    Map<Long, Double> samples = new LinkedHashMap<>(); // LinkedHashMap keeps keys in order as they were inserted
-    for (long at = begin; at < end; at++) {
-      double value = sine1.apply(at) + sine2.apply(at) + (random.nextGaussian() * standardDeviation);
-      if (anomaliesAt.contains(at)) {
-        double min = value / 2;
-        double max = value * 2;
-        value = min + (random.nextDouble() * (max - min) + 1); // nextDouble returns random value between 0.0 and 1.0
+      long numberOfAnomalies = (atEndOfDay - atBeginOfDay) / 100; // 1 percent anomalies
+      List<Long> anomaliesAt = random.longs(numberOfAnomalies, atBeginOfDay, atEndOfDay).boxed()
+          .collect(Collectors.toList());
+
+      Map<Long, Double> samples = new LinkedHashMap<>(); // keeps keys in order as they were inserted
+      for (long at = atBeginOfDay; at <= atEndOfDay; at++) {
+        double value = sine1.apply(at) + sine2.apply(at) + (random.nextGaussian() * standardDeviation);
+        if (anomaliesAt.contains(at)) {
+          double min = value / 2;
+          double max = value * 2;
+          value = min + (random.nextDouble() /* 0.0 < x < 1.0 */ * (max - min) + 1);
+        }
+        samples.put(at, value);
       }
-      samples.put(at, value);
+
+      Path outputFile = outputDir.resolve(curDate + ".con");
+      writeToFile(outputFile, samples);
+      // scilab(samples);
+
+      curDate = curDate.plusDays(1);
     }
-    System.out.print("x=[");
-    // samples.keySet().forEach(k ->  System.out.print(k + ", "));
-    LongStream.range(0, end-begin).forEach(k ->  System.out.print(k + ", "));
-    System.out.println("];");
-    System.out.print("y=[");
-    samples.values().forEach(v ->  System.out.print(v + ", "));
-    System.out.println("];");
   }
 }
