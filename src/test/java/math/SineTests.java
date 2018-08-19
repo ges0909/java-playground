779ecb18968f02789d5295749749d792c8c1d423
Gerrit Schrader <gerrit.schrader@gmail.com>
2018-08-19 12:46:23 +0200

    sine generator impoved

--- src/test/java/math/SineTests.java
+++ src/test/java/math/SineTests.java
@@ -37,7 +37,7 @@ class SineTests {
     System.out.println();
   }
 
-  private void writeToScilabFile(Path outputFile, Map<Long, Double> samples) throws IOException {
+  private void writeToSciLabFile(Path outputFile, Map<Long, Double> samples) throws IOException {
     final StringBuffer xbuf = new StringBuffer();
     LongStream.range(0, samples.size()).forEach(l -> xbuf.append(l).append(","));
     xbuf.setLength(xbuf.length() - 1);
@@ -51,7 +51,7 @@ class SineTests {
     String content = xvar.toString() + "\n" + yvar.toString() + "\n" + "plot(x,y);" + "\n";
 
     Files.write(outputFile, content.toString().getBytes());
-  }
+}
 
   private void writeToFile(Path outputFile, Map<Long, Double> samples) throws IOException {
     StringBuffer sb = new StringBuffer();
@@ -121,40 +121,42 @@ class SineTests {
 
   @Test
   public void testGenerator() throws IOException {
+      final Random random = new Random();
+      final int standardDeviation = 30; // 68%: 3, 95%: 6, >100%: ~9 => use 10 to avoid negative values
+      // @formatter:off
+      final Function<Long, Double> sine1 = (t) -> 200 * Math.sin((2 * Math.PI) /  3600           * t) + 5   + (3 * standardDeviation);
+      final Function<Long, Double> sine2 = (t) -> 300 * Math.sin((2 * Math.PI) / (3600 * 24 * 7) * t) + 300 + (3 * standardDeviation);
+      // @formatter:on
 
-    final Random random = new Random();
-    final int standardDeviation = 3; // 68%: 3, 95%: 6, >100%: ~9 => use 10 to avoid negative values
-    // @formatter:off
-    final Function<Long, Double> sine1 = (t) -> 200 * Math.sin((2 * Math.PI) /  3600           * t) + 5   + (3 * standardDeviation);
-    final Function<Long, Double> sine2 = (t) -> 300 * Math.sin((2 * Math.PI) / (3600 * 24 * 7) * t) + 300 + (3 * standardDeviation);
-    // @formatter:on
+      LocalDate startDate = LocalDate.parse("2018-08-01");
+      LocalDate endDate = LocalDate.parse("2018-08-31");
 
-    LocalDate startDate = LocalDate.parse("2018-08-01");
-    LocalDate endDate = LocalDate.parse("2018-08-31");
+      for (LocalDate date = startDate; date.isBefore(endDate) || date.isEqual(endDate); date = date.plusDays(1)) {
 
-    for (LocalDate curDate = startDate; curDate.isBefore(endDate)
-        || curDate.isEqual(endDate); curDate = curDate.plusDays(1)) {
+          long atBeginningOfDay = date.atTime(LocalTime.MIN).toEpochSecond(ZoneOffset.of("+2"));
+          long atEndOfDay = date.atTime(LocalTime.MAX).toEpochSecond(ZoneOffset.of("+2"));
 
-      long atBeginOfDay = curDate.atTime(LocalTime.MIN).toEpochSecond(ZoneOffset.of("+2"));
-      long atEndOfDay = curDate.atTime(LocalTime.MAX).toEpochSecond(ZoneOffset.of("+2"));
+          long numberOfAnomalies = (atEndOfDay - atBeginningOfDay) / 10_000; // 0,0001 percent anomalies
+          List<Long> anomaliesAt = random.longs(numberOfAnomalies, atBeginningOfDay, atEndOfDay).boxed()
+                  .collect(Collectors.toList());
 
-      long numberOfAnomalies = (atEndOfDay - atBeginOfDay) / 10_000; // 0,0001 percent anomalies
-      List<Long> anomaliesAt = random.longs(numberOfAnomalies, atBeginOfDay, atEndOfDay).boxed()
-          .collect(Collectors.toList());
+          Map<Long, Double> samples = new LinkedHashMap<>(); // keeps keys in order as they were inserted
+          for (long at = atBeginningOfDay; at <= atEndOfDay; at++) {
+              double value = sine1.apply(at) + sine2.apply(at) + (random.nextGaussian() * standardDeviation);
+              if (anomaliesAt.contains(at)) {
+                  int min = 5;
+                  int max = 10;
+                  int anomalizer = random.nextInt((max - min) + 1) + min;
+                  value = random.nextBoolean() ? value * anomalizer : value / anomalizer;
+                  // double min = 0;
+                  // double max = 2 * value;
+                  // value = min + random.nextDouble() * (max - min); // 0.0 < nextDouble() < 1.0
+              }
+              samples.put(at, value);
+          }
 
-      Map<Long, Double> samples = new LinkedHashMap<>(); // keeps keys in order as they were inserted
-      for (long at = atBeginOfDay; at <= atEndOfDay; at++) {
-        double value = sine1.apply(at) + sine2.apply(at) + (random.nextGaussian() * standardDeviation);
-        if (anomaliesAt.contains(at)) {
-          double min = 0;
-          double max = value * 2;
-          value = min + (random.nextDouble() /* 0.0 < x < 1.0 */ * (max - min) + 1);
-        }
-        samples.put(at, value);
+          writeToFile(outputDir.resolve(date + ".con"), samples);
+          writeToSciLabFile(outputDir.resolve(date + ".sci"), samples);
       }
-
-      writeToFile(outputDir.resolve(curDate + ".con"), samples);
-      writeToScilabFile(outputDir.resolve(curDate + ".sci"), samples);
-    }
   }
 }
